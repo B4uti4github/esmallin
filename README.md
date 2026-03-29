bb8105d1bc86c061c1ce08abce5a21ccdb371119
B4uti4github <bautielcyerborgshitpost@gmail.com>
2026-03-28 23:22:49 -0300

    2nd Commit

--- README.md
+++ README.md
@@ -3,6 +3,44 @@
 A framework that makes small improvements to help you develop a better and easier-to-build website.
 
 For now we have this:
-- **EsmallinTranslate**: An class for an better translate like in Flash, but using HTML Elements :D.
+- **Esmallinlash (Before EsmallinTranslate)**: An class for an better translate like in Flash, but using HTML Elements :D.
 
 Anything you want to add here to improve the framework and add more features is welcome ;D.
+# Example
+```html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id="container">
+        <h1>Esmallin</h1>
+    </div>
+    <style>
+        #container {
+            width: 100px;
+            height: 100px;
+            background-color: lightblue;
+            font-family: sans-serif;
+        }
+    </style>
+    <script src="esmallin.js"></script>
+    <script>
+        const esmallinlash = new Esmallinlash();
+
+        var element = document.getElementById('container')
+
+        esmallinlash.add(element, 100, 100)
+        setTimeout(() => {
+            element.x = 500
+            element.rotation = 45
+            element.hue = 180
+        }, 2000)
+        
+    </script>
+</body>
+</html>
+```
--- esmallin.js
+++ esmallin.js
@@ -13,6 +13,7 @@ class EsmallinTranslate {
 
     add(element, x = 0, y = 0) {
         // Inicializamos las propiedades de Flash en el elemento DOM
+        //Transform part
         element.x = x;
         element.y = y;
         element.rotation = 0;
@@ -20,6 +21,12 @@ class EsmallinTranslate {
         element.scaleY = 1;
         element.skewX = 0;
         element.skewY = 0;
+
+        // Filter part
+        element.hue = 0;
+        element.saturation = 1;
+        element.brightness = 1;
+        element.contrast = 1;
 
         // Estilos base para que se comporte como un objeto de Flash
         element.style.position = 'absolute';
@@ -45,6 +52,69 @@ class EsmallinTranslate {
                 rotate(${el.rotation}deg) 
                 scale(${el.scaleX}, ${el.scaleY}) 
                 skew(${el.skewX}deg, ${el.skewY}deg)
+            `;
+            el.style.filter = `
+                hue-rotate(${el.hue}deg) 
+                saturate(${el.saturation}) 
+                brightness(${el.brightness}) 
+                contrast(${el.contrast})
+            `;
+        });
+    }
+}
+class Esmallinlash {
+    constructor() {
+        this.list = [];
+        this.start()
+    }
+
+    add(element, x = 0, y = 0) {
+        // Inicializamos las propiedades de Flash en el elemento DOM
+        //Transform part
+        element.x = x;
+        element.y = y;
+        element.rotation = 0;
+        element.scaleX = 1;
+        element.scaleY = 1;
+        element.skewX = 0;
+        element.skewY = 0;
+
+        // Filter part
+        element.hue = 0;
+        element.saturation = 1;
+        element.brightness = 1;
+        element.contrast = 1;
+
+        // Estilos base para que se comporte como un objeto de Flash
+        element.style.position = 'absolute';
+        element.style.transformOrigin = '0 0'; 
+        
+        this.list.push(element);
+    }
+
+    start() {
+        // Usamos bind para que "this" dentro de render siga siendo la clase
+        const _loop = () => {
+            this.render();
+            requestAnimationFrame(_loop);
+        };
+        requestAnimationFrame(_loop);
+    }
+
+    render() {
+        this.list.forEach(el => {
+            // Combinamos todo en una sola operación de GPU
+            el.style.transform = `
+                translate(${el.x}px, ${el.y}px) 
+                rotate(${el.rotation}deg) 
+                scale(${el.scaleX}, ${el.scaleY}) 
+                skew(${el.skewX}deg, ${el.skewY}deg)
+            `;
+            el.style.filter = `
+                hue-rotate(${el.hue}deg) 
+                saturate(${el.saturation}) 
+                brightness(${el.brightness}) 
+                contrast(${el.contrast})
             `;
         });
     }
